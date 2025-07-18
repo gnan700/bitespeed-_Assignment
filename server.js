becb04e069e8e58dffbb9c15544cf983f3cce13d
gnan700 <87974726+gnan700@users.noreply.github.com>
2025-07-18 13:48:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const helmet = require("helmet")
+const routes = require("./app/routes")
+require("dotenv").config()
+
+const app = express();
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'));
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}!`))
